8bc55eeffa30184c80b1a8ce237345edb1d4993b
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-12 15:46:39 +0200

    callback

--- src/dataLoader.ts
+++ src/dataLoader.ts
@@ -1,10 +1,20 @@
 import axios from 'axios';
+import { IEmployee } from './interfaces';
 
 export const ColorDataLoader = () => {
 	return ['green', 'red', 'blue', 'yellow'];
 }
 
-export const EmployeeDataLoader = async () => {
+export const EmployeeDataLoader = (callback: (employees: IEmployee[]) => void) => {
+	(async () => {
+		const url = 'https://edwardtanguay.vercel.app/share/employees.json';
+		const response = await axios.get(url);
+		const employees = response.data;
+		callback(employees);
+	})();
+}
+
+export const EmployeeDataLoader2 = async () => {
 	const url = 'https://edwardtanguay.vercel.app/share/employees.json';
 	const response = await axios.get(url);
 	const employees = response.data;
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import './style.css';
 import { ColorDataLoader, EmployeeDataLoader } from './dataLoader';
+import { IEmployee } from './interfaces';
 
 const colors = ColorDataLoader();
-const employees = await EmployeeDataLoader();
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-  <h1>Callback/Promises</h1>
-  <p>The colors are: ${colors.join(', ')}</p>
-  <p>There are ${employees.length} employees</p>
-  </div>
+EmployeeDataLoader((employees: IEmployee[]) => {
+	document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+	<div>
+	<h1>Callback/Promises</h1>
+	<p>The colors are: ${colors.join(', ')}</p>
+	<p>There are ${employees.length} employees</p>
+	</div>
 `
+});
