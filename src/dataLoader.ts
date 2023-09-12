2c73f00be9764093b176f5ab492965bf7d7b7454
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-12 15:28:05 +0200

    works as blocking

--- src/dataLoader.ts
+++ src/dataLoader.ts
@@ -1,4 +1,15 @@
+import axios from 'axios';
+
 export const ColorDataLoader = () => {
-  return ['green', 'red', 'blue', 'yellow'];
+	return ['green', 'red', 'blue', 'yellow'];
 }
+
+export const EmployeeDataLoader = async () => {
+	const url = 'https://edwardtanguay.vercel.app/share/employees.json';
+	const response = await axios.get(url);
+	const employees = response.data;
+	return employees;
+}
+
+
 
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import './style.css';
-import { ColorDataLoader } from './dataLoader';
+import { ColorDataLoader, EmployeeDataLoader } from './dataLoader';
 
 const colors = ColorDataLoader();
+const employees = await EmployeeDataLoader();
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
   <h1>Callback/Promises</h1>
   <p>The colors are: ${colors.join(', ')}</p>
+  <p>There are ${employees.length} employees</p>
   </div>
 `
