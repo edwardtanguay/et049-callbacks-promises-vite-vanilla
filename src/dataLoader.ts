b1c58c74495feab26f2c0763641e50e51c84ae4a
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-13 13:55:13 +0200

    finished update area

--- src/dataLoader.ts
+++ src/dataLoader.ts
@@ -6,6 +6,25 @@ export const ColorDataLoader = () => {
 }
 
 export const EmployeeDataLoader = (callback: (employees: IEmployee[]) => void) => {
+	setTimeout(() => {
+		callback([
+			{
+				firstName: 'fff',
+				lastName: 'lll'
+			},
+			{
+				firstName: 'fff',
+				lastName: 'lll'
+			},
+			{
+				firstName: 'fff',
+				lastName: 'lll'
+			}
+		])
+	}, 1000)
+}
+
+export const EmployeeDataLoader3 = (callback: (employees: IEmployee[]) => void) => {
 	(async () => {
 		const url = 'https://edwardtanguay.vercel.app/share/employees.json';
 		const response = await axios.get(url);
@@ -23,3 +42,24 @@ export const EmployeeDataLoader2 = async () => {
 
 
 
+
+export const EmployeeDataLoader5 = (callback: (employees: IEmployee[]) => void) => {
+	const getRandomNumber = (begin:number, end:number) => {
+		const range = end - begin + 1;
+		return Math.floor(Math.random() * range) + begin;
+	};
+
+	const emps: IEmployee[] = []
+	for (let i = 1; i <= getRandomNumber(3,10); i++) {
+		emps.push(
+			{
+				firstName: 'fff',
+				lastName: 'lll'
+			},
+		)
+	}
+
+	setTimeout(() => {
+		callback(emps);
+	}, 1000)
+}
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import './style.css';
-import { ColorDataLoader, EmployeeDataLoader } from './dataLoader';
+import { EmployeeDataLoader, EmployeeDataLoader5 } from './dataLoader';
 import { IEmployee } from './interfaces';
 
-const colors = ColorDataLoader();
+document.querySelector<HTMLButtonElement>('#btnChange')!.addEventListener('click', () => {
+	EmployeeDataLoader5((employees: IEmployee[]) => {
+		document.querySelector<HTMLSpanElement>('#employeeNumber')!.innerHTML = `
+	${employees.length}
+`
+	});
+})
+
 EmployeeDataLoader((employees: IEmployee[]) => {
-	document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-	<div>
-	<h1>Callback/Promises</h1>
-	<p>The colors are: ${colors.join(', ')}</p>
-	<p>There are ${employees.length} employees</p>
-	</div>
+	document.querySelector<HTMLSpanElement>('#employeeNumber')!.innerHTML = `
+	${employees.length}
 `
 });
